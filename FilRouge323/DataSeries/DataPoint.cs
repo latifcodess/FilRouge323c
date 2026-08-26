1183b6e97e66c6b31db94c9ba75c424c8bc23901
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-08-26 09:56:38 +0200

    ajout du code

---
+++ FilRouge323/DataSeries/DataPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace DataSeries;
+
+public class DataPoint<T>
+{
+    public DateTime Timestamp { get; }
+    public T Value { get; }
+
+    public DataPoint(DateTime timestamp, T value)
+    {
+        Timestamp = timestamp;
+        Value = value;
+    }
+}
---
+++ FilRouge323/FilRouge323/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FilRouge323
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+    public class ValorantMatch
+    {
+        public string Player { get; }
+        public string Agent { get; }
+        public int Kills { get; }
+        public int Deaths { get; }
+        public int Assists { get; }
+        public int Headshots { get; }
+        public int RoundsWon { get; }
+        public bool Won { get; }
+
+        public ValorantMatch(string player, string agent, int kills, int deaths,
+            int assists, int headshots, int roundsWon, bool won)
+        {
+            Player = player;
+            Agent = agent;
+            Kills = kills;
+            Deaths = deaths;
+            Assists = assists;
+            Headshots = headshots;
+            RoundsWon = roundsWon;
+            Won = won;
+        }
+    }
+}
